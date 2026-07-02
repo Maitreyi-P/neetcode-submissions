b889d867ae4a3bd59d935ab8fb3fc40817bd6308
Maitreyi Poornachandra <maitreyi2405@gmail.com>
2026-07-02 13:50:07 -0700

    Add: palindrome-number - submission-5

---
+++ Data Structures & Algorithms/palindrome-number/submission-5.py
@@ -0,0 +1,21 @@
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        
+        if x < 0:
+            return False
+        if 0 <= x <= 9:
+            return True
+
+        s = str(x)
+        l = 0
+        r = len(s) - 1
+
+        while l <= r:
+            if s[l] != s[r]:
+                return False
+
+            l += 1
+            r -= 1
+        
+        return True
+        
