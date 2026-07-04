6974e2ac6137ff801e8a8005ad30bc40c9793049
Maitreyi Poornachandra <maitreyi2405@gmail.com>
2026-07-04 12:23:34 -0700

    Update: anagram-groups - submission-3

--- Data Structures & Algorithms/anagram-groups/submission-3.py
+++ Data Structures & Algorithms/anagram-groups/submission-3.py
@@ -1,13 +1,16 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        hmap = {}
-        for i in range(len(strs)):
-            # print(''.join(sorted(strs[i])))
-            if ''.join(sorted(strs[i])) not in hmap:
-                hmap[''.join(sorted(strs[i]))] = [strs[i]]
-            else:
-                hmap[''.join(sorted(strs[i]))] += [strs[i]]
-        ans =[]
-        for i in hmap:
-            ans.append(hmap[i])
-        return ans
+        
+        d = defaultdict(list)
+
+        for s in strs:
+            ordstr = [0] * 26
+            for i in s:
+                ordstr[ord(i) - ord('a')] += 1
+            d[str(ordstr)].append(s)
+
+        res = []
+        for key, val in d.items():
+            res.append(val)
+
+        return res
